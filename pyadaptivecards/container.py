9c1acc678fd9ed83c3ee5738f762264d5f9d5cad
Neidinger, Marcel <mneiding@cisco.com>
2019-11-01 12:07:15 +0100

    Added docstrings to containers

--- pyadaptivecards/container.py
+++ pyadaptivecards/container.py
@@ -23,85 +23,136 @@ SOFTWARE.
 """
 
 from .abstract_components import Serializable
+from .options import ImageSize
 
 class Container(Serializable):
-    def __init__(self, items, selectAction=None,
-                              style=None,
-                              verticalContentAlignment=None,
+   """Groups items together."""
+   def __init__(self, items, selectAction=None,
+                             style=None,
+                             verticalContentAlignment=None,
+                             height=None,
+                             separator=None,
+                             spacing=None,
+                             id=None):
+      """Create a new container.
+
+      Args:
+         items(list): List of items this container should contain.
+         selectAction(OpenURL, Submit): Action to be invoked when container is selected.
+         style(ContainerStyle): Style of this container.
+         verticalContentAlignment(VerticalContentAlignment): Specifies vertical alignment 
+            of the content. 
+         height(BlockElementHeight): Specifies the way the height of this container should 
+            be calculated (stretch or auto).
+         separator(bool): Draw a separating line when set to true
+         spacing(Spacing): Specify the spacing of this component
+         id(str): The id of this component
+      """
+      self.type = "Container"
+      self.items = items
+      self.selectAction = selectAction
+      self.style = style
+      self.verticalContentAlignment = verticalContentAlignment
+      self.height = height
+      self.separator = separator
+      self.spacing = spacing
+      self.id = id
+
+      super().__init__(serializable_properties=['items'],
+                       simple_properties=[
+                           'selectAction', 'style', 'verticalContentAlignment',
+                           'height', 'separator', 'spacing', 'id', 'type'
+                       ])
+
+class ColumnSet(Serializable):
+   """Set of columns to display content side by side."""
+   def __init__(self, columns=None,
+                      selectAction=None,
+                      height=None,
+                      separator=None,
+                      spacing=None,
+                      id=None):
+      """Create a new ColumnSet.
+
+      Args:
+         columns(list): List of Column elements.
+         selectAction(OpenURL, Submit): Action to be invoked when container is selected.
+         height(BlockElementHeight): Specifies the way the height of this container should 
+            be calculated (stretch or auto).
+         separator(bool): Draw a separating line when set to true
+         spacing(Spacing): Specify the spacing of this component
+         id(str): The id of this component
+      """
+      self.type = "ColumnSet"
+      self.columns = columns
+      self.selectAction = selectAction
+      self.height = height
+      self.separator = separator
+      self.spacing = spacing
+      self.id = id
+
+      super().__init__(serializable_properties=['columns'],
+                       simple_properties=[
+                        'selectAction', 'height', 'separator', 'spacing',
+                        'id', 'type'
+                      ])
+
+class FactSet(Serializable):
+   """Set of facts to create a factoide."""
+   def __init__(self, facts, height=None,
+                             separator=None,
+                             spacing=None,
+                             id=None):
+      """Create a new FactSet.
+
+      Args:
+         facts(list): List of Fact Elements to be displayed.
+         height(BlockElementHeight): Specifies the way the height of this container should 
+            be calculated (stretch or auto).
+         separator(bool): Draw a separating line when set to true
+         spacing(Spacing): Specify the spacing of this component
+         id(str): The id of this component
+      """
+      self.type = "FactSet"
+      self.facts = facts
+      self.height = height
+      self.separator = separator
+      self.spacing = spacing
+      self.id = id
+
+      super().__init__(serializable_properties=['facts'],
+                       simple_properties=[
+                           'type', 'height', 'separator', 'id', 'spacing'
+                       ])
+
+class ImageSet(Serializable):
+   def __init__(self, images, imageSize=None,
                               height=None,
                               separator=None,
                               spacing=None,
                               id=None):
-        self.type = "Container"
-        self.items = items
-        self.selectAction = selectAction
-        self.style = style
-        self.verticalContentAlignment = verticalContentAlignment
-        self.height = height
-        self.separator = separator
-        self.spacing = spacing
-        self.id = id
+      """Create a new ImageSet.
+      
+      Args:
+         images(list): List of Image objects to be displayed in a image set.
+         imageSize(ImageSize): Size of the image.
+         height(BlockElementHeight): Specifies the way the height of this container should 
+            be calculated (stretch or auto).
+         separator(bool): Draw a separating line when set to true
+         spacing(Spacing): Specify the spacing of this component
+         id(str): The id of this component
+      """
+      
+      self.type = "ImageSet"
+      self.images = images
+      self.imageSize = imageSize
+      self.height = height
+      self.separator = separator
+      self.spacing = spacing
+      self.id = id
 
-        super().__init__(serializable_properties=['items'],
-                         simple_properties=[
-                            'selectAction', 'style', 'verticalContentAlignment',
-                            'height', 'separator', 'spacing', 'id', 'type'
-                         ])
-
-class ColumnSet(Serializable):
-    def __init__(self, columns=None,
-                       selectAction=None,
-                       height=None,
-                       separator=None,
-                       spacing=None,
-                       id=None):
-        self.type = "ColumnSet"
-        self.columns = columns
-        self.selectAction = selectAction
-        self.height = height
-        self.separator = separator
-        self.spacing = spacing
-        self.id = id
-
-        super().__init__(serializable_properties=['columns'],
-                         simple_properties=[
-                            'selectAction', 'height', 'separator', 'spacing',
-                            'id', 'type'
-                         ])
-
-class FactSet(Serializable):
-    def __init__(self, facts, height=None,
-                              separator=None,
-                              spacing=None,
-                              id=None):
-        self.type = "FactSet"
-        self.facts = facts
-        self.height = height
-        self.separator = separator
-        self.spacing = spacing
-        self.id = id
-
-        super().__init__(serializable_properties=['facts'],
-                         simple_properties=[
-                            'type', 'height', 'separator', 'id', 'spacing'
-                         ])
-
-class ImageSet(Serializable):
-    def __init__(self, images, imageSize=None,
-                               height=None,
-                               separator=None,
-                               spacing=None,
-                               id=None):
-        self.type = "ImageSet"
-        self.images = images
-        self.imageSize = imageSize
-        self.height = height
-        self.separator = separator
-        self.spacing = spacing
-        self.id = id
-
-        super().__init__(serializable_properties=['images'],
-                         simple_properties=[
-                            'imageSize', 'height', 'separator', 'spacing', 'id',
-                            'type'
-                        ])
+      super().__init__(serializable_properties=['images'],
+                       simple_properties=[
+                           'imageSize', 'height', 'separator', 'spacing', 'id',
+                           'type'
+                      ])
